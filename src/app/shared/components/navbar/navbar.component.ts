8a06c2932d10fa69613ca52669f36c43a6b9158a
Cartago <cartago.relaxingcup@gmail.com>
2022-10-24 23:54:22 +0200

    Creamos la primera card con la información relacionada al curso

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  public title: string = 'Redux en Angular - NGRX - Por Fernando Herrera'
-  constructor() { }
+  public title: string = 'Redux en Angular - NGRX - Por Fernando Herrera';
 
-  ngOnInit(): void {
+  constructor(private _router: Router) {}
+
+  ngOnInit(): void {}
+
+  public logoClicked(): void {
+    this._router.navigate(['/']);
   }
-
 }
